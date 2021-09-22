3165e53447a1237e7112966527b5298f650fb367
Waqas Tahir <wakaztahir@gmail.com>
2021-09-22 14:38:30 +0500

    boom

--- 1-twosum/twosum.cpp
+++ 1-twosum/twosum.cpp
@@ -12,19 +12,14 @@
  * @return : pair of pointers to indices of two numbers , -1,-1 if no solution found
  */
 std::pair<int, int> find_two_sum(const int numbers[], int size, int target) {
-    int index1 = -1;
-    int index2 = -1;
-
     for (int i = 0; i < size; i++) {
         for (int j = i + 1; j < size; j++) {
             int number_to_find = target - numbers[i];
             if (numbers[j] == number_to_find) {
-                index1 = i;
-                index2 = j;
+                return std::make_pair(i, j);
             }
         }
     }
 
-
-    return std::make_pair(index1, index2);
+    return std::make_pair(-1, -1);
 }
