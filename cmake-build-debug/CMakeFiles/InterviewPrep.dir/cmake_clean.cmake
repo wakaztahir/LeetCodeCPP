804f0beb76bf183bde82ef89451e45d14bbdb08d
Waqas Tahir <wakaztahir@gmail.com>
2021-09-22 13:52:47 +0500

    init

---
+++ cmake-build-debug/CMakeFiles/InterviewPrep.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InterviewPrep.dir/main.cpp.obj"
+  "InterviewPrep.exe"
+  "InterviewPrep.exe.manifest"
+  "InterviewPrep.lib"
+  "InterviewPrep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InterviewPrep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
