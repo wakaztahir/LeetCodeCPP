ad425a56e32cb8cff9ea95ef0146698e2464c70c
Waqas Tahir <wakaztahir@gmail.com>
2021-09-30 16:18:07 +0500

    reverse linked list between indices

--- 7-reverse-linked-list/reverse-linked-list.cpp
+++ 7-reverse-linked-list/reverse-linked-list.cpp
@@ -15,3 +15,32 @@ ListNode *reverseList(ListNode *head) {
     }
     return previous;
 }
+
+ListNode *reverseList(ListNode *head, int left, int right) {
+    auto current = head;
+    ListNode *start = head;
+    int position = 1;
+    while (position < left) {
+        auto next = current->next;
+        start = current;
+        current = next;
+        position++;
+    }
+    ListNode *newList = nullptr;
+    ListNode *tail = current;
+    while (position >= left && position <= right) {
+        // reversing
+        auto next = current->next;
+        current->next = newList;
+        newList = current;
+        current = next;
+        position++;
+    }
+    start->next = newList;
+    tail->next = current;
+    if(left>1) {
+        return head;
+    }else{
+        return newList;
+    }
+}
--- 7-reverse-linked-list/reverse-linked-list.h
+++ 7-reverse-linked-list/reverse-linked-list.h
@@ -23,4 +23,13 @@ struct ListNode {
  */
 ListNode *reverseList(ListNode *head);
 
+/**
+ * \LeetCode https://leetcode.com/problems/reverse-linked-list-ii/
+ * @param head , head of linked list
+ * @param left , start of reversal
+ * @param right , end of reversal
+ * @return linked list with values from left to right reversed
+ */
+ListNode *reverseList(ListNode *head,int left,int right);
+
 #endif //INTERVIEWPREP_REVERSE_LINKED_LIST_H
